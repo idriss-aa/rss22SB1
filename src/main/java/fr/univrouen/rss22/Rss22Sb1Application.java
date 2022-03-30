9ccb46c273c7c7c9436ef3c994112a72189ca7fc
aazza idriss <idrissaazza@gmail.com>
2022-03-30 09:43:22 +0200

    first commit

---
+++ src/main/java/fr/univrouen/rss22/Rss22Sb1Application.java
@@ -0,0 +1,13 @@
+package fr.univrouen.rss22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rss22Sb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rss22Sb1Application.class, args);
+	}
+
+}
---
+++ src/test/java/fr/univrouen/rss22/Rss22Sb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.univrouen.rss22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rss22Sb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
